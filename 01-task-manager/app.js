48fcf556a6ac4641b616a2d577a63cd912d92e5b
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-01-09 15:48:10 +0100

    Add not found middleware

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 require('dotenv').config();
+const notFound = require('./middleware/notFound');
 
 app.use(express.static('./public'));
 // middleware
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
 
 app.listen(5000, () => {
   console.log(`Le serveur écoute sur le port 5000...`);
