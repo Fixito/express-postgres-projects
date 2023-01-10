2a7631c1ced2f7695c40fc79e180487205640779
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-01-10 14:26:52 +0100

    Setup API

--- 02-store-api/app.js
+++ 02-store-api/app.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
+//* async errors
+// nous évite d'avoir à refaire notre async middleware
+require('express-async-errors');
+
 const express = require('express');
 const app = express();
 
-app.get('/', async (req, res) => {
-  res.send('hello world');
+const notFoundMiddleware = require('./middleware/notFound');
+const errorMiddleware = require('./middleware/errorHandler');
+
+const products = require('./routes/products');
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use('/api/v1/products', products);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 app.listen(5000, () => {
   console.log('Le serveur écoute sur le port 5000...');
